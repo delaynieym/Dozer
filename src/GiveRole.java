9283a849fb0d7ad9c188bce3ffe4b4b6f9fbc11e
Delaynie McMillan <delaynieym@outlook.com>
2019-05-07 07:30:52 -0700

    checks if role requested and user have admin permissions

--- src/GiveRole.java
+++ src/GiveRole.java
@@ -1,24 +1,39 @@
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class GiveRole extends ListenerAdapter{
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
+		Member member = event.getMember();
 		String[] args = event.getMessage().getContentRaw().split("\\s+");
 		
+// parsing request
 		if (args[0].equalsIgnoreCase(Bot.prefix + "give") && args.length > 1) {
-			String role = "";
+			String request = "";
 			for (int i = 1; i < args.length; i++) {
-				role += args[i];
-				if (i < args.length - 1) role += " ";
+				request += args[i];
+				if (i < args.length - 1) request += " ";
 			}
 			
+// getting the Role
+			Role role = null;
 			try {
-				event.getGuild().getController().addRolesToMember(event.getMember(), event.getJDA().getRolesByName(role, true)).complete();
+				role = event.getJDA().getRolesByName(request, true).get(0);
 			}
 			catch (Exception e) {
 				event.getChannel().sendMessage("Exception: " + e).queue();
 			}
+			
+			if (role.hasPermission(Permission.ADMINISTRATOR) && !member.hasPermission(Permission.ADMINISTRATOR)) {
+				event.getChannel().sendMessage("sorry " + member.getAsMention() + ", you don't have permission to hold that role").queue();
+			}
+			
+			event.getGuild().getController().addRolesToMember(member, role).complete();
+			event.getChannel().sendMessage(member.getAsMention() + " role successfully added").queue();
+
 		}
 	}
 }
